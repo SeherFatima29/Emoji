52bef7ac2f950d6f394e8aaed2d9da1fba09016b
seherfatima <156211654+SeherFatima29@users.noreply.github.com>
2024-03-06 16:57:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+var ask=prompt("which emoji")
+if (ask == "smile"){
+    alert("😊")
+}
+else if(ask == "upset"){
+alert("😐")
+}
+else if (ask == "happy"){
+    alert("😆")
+}
+else if(ask == "Funny"){
+alert("😅")
+}
+else if (ask == "yummy"){
+    alert("😋")
+}
+else if(ask == "angry"){
+alert("😠")
+}
+else if(ask == "cry"){
+    alert("😭")
+}
+else if(ask == "tear"){
+    alert("😢")
+}
+else if(ask == "sleep"){
+    alert("😴")
+}
+else if(ask == "surprise"){
+    alert("😲")
+}
+else if(ask == "think"){
+    alert("🤔")
+}
+else if(ask == "cold"){
+    alert("🥶")
+}
+else if(ask == "worried"){
+    alert("😟")
+}
+else if(ask == "shock"){
+    alert("🤯")
+}
